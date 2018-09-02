67a0fa08036ccaa5bfdb9deb4b2dd2fcdd320ddd
charleyustccs <charleyustccs@gmail.com>
2018-09-02 11:37:56 +0800

    nav fix

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,8 @@ var next = document.getElementById("next");
 next.onclick = function () {
   alert('d');
 }
+
+var logo = document.getElementById("logo");
+logo.onclick = function () {
+  alert('Welcome to home');
+}
